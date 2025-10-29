6d7cf24fe4107dc93f74334cb2b1b408d0777c9d
oliverstivers <oliverstivers@gmail.com>
2025-10-28 23:58:24 -0700

    added license

--- scanner.py
+++ scanner.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2025 Oliver Stivers
+# Licensed under the MIT License. See LICENSE file in the project root for full license text.
+
 from pupil_apriltags import Detector
 import cv2
 
--- task_manager.py
+++ task_manager.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2025 Oliver Stivers
+# Licensed under the MIT License. See LICENSE file in the project root for full license text.
+
 from task import Task
 import datetime
 
@@ -6,5 +9,3 @@ task1 = Task("Do laundry", category="chores", due_date=None, parentID=None)
 
 receipt = task1.get_receipt()
 print("\n".join(receipt))
-
-
