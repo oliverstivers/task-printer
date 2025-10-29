73e6b98e6538e514f6be4b8875ab327066084018
oliverstivers <oliverstivers@gmail.com>
2025-10-29 10:47:37 -0700

    file saving working

--- task_manager.py
+++ task_manager.py
@@ -3,9 +3,26 @@
 
 from task import Task
 import datetime
+import pickle
 
 
-task1 = Task("Do laundry", category="chores", due_date=None, parentID=None)
 
-receipt = task1.get_receipt()
-print("\n".join(receipt))
+class TaskManager:
+    tasks = []
+    task1 = Task("Do laundry", category="chores", due_date=None, parentID=None)
+    tasks.append(task1)
+
+    def save_tasks_to_file(tasks):
+        with open("tasks.pkl", "wb") as f:
+            pickle.dump(tasks, f)
+
+
+        
+if __name__ == "__main__":
+    with open("tasks.pkl", "rb") as f:
+        tasks: list[Task] = pickle.load(f)
+
+    for task in tasks:
+        print("\n".join(task.get_receipt()))
+
+        
