d4c444b06d422f80ec0a54fbbcbe2c1462ce8970
ostive <ostive@uw.edu>
2025-10-28 23:53:05 -0700

    code features added

--- task.py
+++ task.py
@@ -1,4 +1,6 @@
-import datetime
+import datetime, timedelta
+import tabulate
+import uuid
 
 
 # Each task will represent something i have to do
@@ -8,13 +10,51 @@ import datetime
 # we should keep track of a print time that controls when the task is printed, can be set explicitly or automatically
 class Task:
     _due_date = None
-    _duration = None
+    _duration = 0
     _task_id = None
     _print_time = None
+    _task_name = None
+    _task_category = ""
+    _parent_id = None
+    _child_id = None
 
-    def __init__(self, due_date):
-        _due_date = due_date
-    
+    def __init__(self, name, due_date, category, parentID, duration=0):
+        if due_date is None:
+            self._due_date = datetime.datetime.now() + datetime.timedelta(minutes=5)
+        else:
+            self._due_date = due_date
+        self._task_id = uuid.uuid4()
+        self._task_category = category
+        self._task_name = name
 
-    def getUnusedTaskId():
-        
+    def get_receipt(self):
+        table = [
+            ["Name", self._task_name],
+            ["ID", self._task_id],
+            ["Due date: ", self.get_due_date()],
+        ]
+
+        # todo: figure out task tree
+
+        category = self._task_category
+
+        lines = [
+            f"{self._task_name.center(60)}",
+            "=" * 60,
+            "",
+            f"{'Due:'.ljust(12)}{self.get_due_date()}",
+            f"\n{'Time:'.ljust(12)}{self._due_date.strftime("%H:%M")}\n"
+            f"\n{'Category:'.ljust(12)}{category}\n" if category is not None else "",
+            
+            "-" * 60,
+            "",
+            f"{str(self._task_id)[:8]}",
+            "",
+        ]
+        return lines
+
+    def get_due_date(self):
+        if self._due_date is None:
+            return "No deadline"
+
+        return self._due_date.strftime("%A, %d %B")
--- task_manager.py
+++ task_manager.py
@@ -1,5 +1,10 @@
-import task
+from task import Task
+import datetime
 
-current_tasks = []
-used_ids
+
+task1 = Task("Do laundry", category="chores", due_date=None, parentID=None)
+
+receipt = task1.get_receipt()
+print("\n".join(receipt))
+
 
