18370fab4366f4c6644c1f491610614eea49954b
oliverstivers <oliverstivers@gmail.com>
2025-10-30 13:15:54 -0700

    work on tag assignment and such

--- task_manager.py
+++ task_manager.py
@@ -5,13 +5,14 @@ from task import Task
 import datetime
 import pickle
 from treelib import Tree
-
-
+import random
 
 
 class TaskManager:
     tasks: list[Task] = []
-    
+    task_tag_map: {int, Task} = {}
+    used_ids = set()
+
     task1 = Task("Do laundry", category="chores", due_date=None)
     task2 = Task("essay project", category="esrm", due_date=datetime.date(2025, 11, 2))
     task3 = Task("plan essay", category="esrm", due_date=datetime.date(2025, 10, 30))
@@ -21,8 +22,6 @@ class TaskManager:
     task2.add_child(task4)
     task3.add_child(task5)
 
-
-    
     tasks.append(task1)
     tasks.append(task2)
     tasks.append(task3)
@@ -37,16 +36,26 @@ class TaskManager:
         tree = Tree()
         tree.create_node("Root", "root")
         for task in TaskManager.tasks:
-            tree.create_node(task._task_name, task._task_id, parent=task._parent_id if task._parent_id is not None else "root")
-        
+            tree.create_node(
+                task._task_name,
+                task._task_id,
+                parent=task._parent_id if task._parent_id is not None else "root",
+            )
+
         tree.show()
 
+    def assign_tags():
+        for task in TaskManager.tasks:
+            if(task._status == Task.Status.TODO):
+                id = random.randint(0, 300)
+                if(id not in TaskManager.used_ids):
+                    TaskManager.task_tag_map[id] = task
+                    TaskManager.used_ids.add(id)
 
-    
 
-
-        
 if __name__ == "__main__":
     TaskManager.build_tree()
-
-        
+    TaskManager.assign_tags()
+    for id in TaskManager.used_ids:
+        task: Task = TaskManager.task_tag_map[id]
+        print(str(id) + "\n".join(task.get_receipt()))
